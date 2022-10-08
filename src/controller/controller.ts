523b4d831d3f0c868799484c102032e51bff3147
Allen <zifeng.allen@gmail.com>
2022-10-07 21:42:54 -0400

    standardize response

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -1,55 +1,62 @@
 import {NextFunction, Request, Response} from "express";
-import { PrismaClient } from '@prisma/client'
+import {PrismaClient} from '@prisma/client'
 import bcrypt from "bcrypt";
 
 const salt = bcrypt.genSaltSync(2);
 
-let register_middleware = async (req: Request, res: Response, next:NextFunction) => {
+let response = (status: number, message: string) => {
+    return {"status": status,
+            "message":message
+             }
+}
+
+
+let register_middleware = async (req: Request, res: Response, next: NextFunction) => {
 
     console.log("inside register_middleware")
 
     const prisma = new PrismaClient()
-    try{
+    try {
         const user = await prisma.user.create({
             data: {
                 email: req.body.email,
                 password: bcrypt.hashSync(req.body.pwd, salt) // hashed pwd
             }
         })
-        res.json(req.body)
+        res.status(200).json( response(200,"Successful Registration!"))
         console.log("success register_middleware")
-    } catch (err){
-        console.error("Error!!!!!!!!!!!!!!!",err)
-        res.json(err)
+    } catch (err) {
+        console.error("Error!!!!!!!!!!!!!!!", err)
+        res.status(409).json( response(409,"Error! Email Already Exist!"))
     }
 
 
 }
-let login_middleware = async (req: Request, res: Response, next:NextFunction) => {
+let login_middleware = async (req: Request, res: Response, next: NextFunction) => {
     console.log("inside login_middleware")
 
     const prisma = new PrismaClient()
-    try{
+    try {
         const user = await prisma.user.findUnique({
             where: {
                 email: req.body.email,
             }
         })
-        if (user!= null && bcrypt.compareSync(req.body.pwd, user.password)){
+        if (user != null && bcrypt.compareSync(req.body.pwd, user.password)) {
             console.log("successful login")
-            res.json(user)
-        }else{
+            res.status(200).json( response(200,"Successful Login!"))
+        } else {
             console.log("failed login")
-            res.json({'error':'error'})
+            res.status(401 ).json( response(401,"Error! Unauthorized!"))
         }
 
-    } catch (err){
-        console.error("Error!!!!!!!!!!!!!!!",err)
-        res.json(err)
+    } catch (err) {
+        console.error("Error!!!!!!!!!!!!!!!", err)
+        res.status(401).json( response(401,"Error!"))
     }
 }
 
-export {register_middleware,login_middleware};
+export {register_middleware, login_middleware};
 
 // let login_middleware = (req: Request, res: Response, next:NextFunction) => {
 //     console.log("f2 received requests")
